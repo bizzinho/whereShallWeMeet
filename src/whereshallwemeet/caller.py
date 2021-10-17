82bcd3c8c19a7557193a35084e8fa0b5fa599f78
Dave Ochsenbein <dave.ochsenbein@gmail.com>
2021-10-17 20:30:02 +0200

    made to class to have connection persistence; added two fields availableToHost and joinsParty in yaml/csv

--- src/whereshallwemeet/caller.py
+++ src/whereshallwemeet/caller.py
@@ -7,131 +7,155 @@ from datetime import datetime as dt
 from typing import Union, Tuple, List
 
 
-def _loadFriends(friendsFile: str):
-    path = pathlib.Path(friendsFile)
+class WhereShallWeMeet:
+    def __init__(self, configPath: str = None):
 
-    if path.suffix == ".csv":
-        import csv
+        self.configPath = configPath
 
-        friends = []
-        with open(friendsFile, "r") as f:
-            csvfile = list(csv.reader(f))
+        self._gmaps = None
 
-        # first row is header
-        header = csvfile[0]
+        self.friends = None
 
-        def findCol(word):
-            shoeFits = list(
-                filter(
-                    lambda i: [word in col for col in header][i],
-                    range(len(header)),
+    @property
+    def gmaps(self):
+
+        if self._gmaps is None:
+            self._gmaps = self._establishConnection(self.configPath)
+
+        return self._gmaps
+
+    def loadFriends(self, friendsFile: str):
+        path = pathlib.Path(friendsFile)
+
+        if path.suffix == ".csv":
+            import csv
+
+            friends = []
+            with open(friendsFile, "r") as f:
+                csvfile = list(csv.reader(f))
+
+            # first row is header
+            header = csvfile[0]
+
+            def findCol(word):
+                shoeFits = list(
+                    filter(
+                        lambda i: [word in col.lower() for col in header][i],
+                        range(len(header)),
+                    )
                 )
-            )
 
-            if len(shoeFits) > 1:
-                raise ValueError(
-                    f"More than one column header contains {word}."
+                if len(shoeFits) > 1:
+                    raise ValueError(
+                        f"More than one column header contains {word}."
+                    )
+                elif len(shoeFits) == 0:
+                    raise ValueError(
+                        f"No column header contains keyword {word}"
+                    )
+
+                return shoeFits[0]
+
+            nameCol = findCol("name")
+            addressCol = findCol("address")
+            transitCol = findCol("preferred")
+            hostCol = findCol("host")
+            joinsCol = findCol("joins")
+
+            for row in csvfile[1:]:
+
+                friends.append(
+                    {
+                        "name": row[nameCol],
+                        "address": row[addressCol],
+                        "preferredTransitMode": row[transitCol],
+                        "availableToHost": row[hostCol],
+                        "joinsParty": row[joinsCol],
+                    }
                 )
-            elif len(shoeFits) == 0:
-                raise ValueError(f"No column header contains keyword {word}")
+        elif (path.suffix == ".yaml") or (path.suffix == ".yml"):
+            import yaml
 
-            return shoeFits[0]
+            with open(friendsFile, "r") as f:
+                ff = yaml.load(f, Loader=yaml.FullLoader)
 
-        nameCol = findCol("name")
-        addressCol = findCol("address")
-        transitCol = findCol("preferred")
+            friends = ff["friends"]
 
-        for row in csvfile[1:]:
+        # remove people that don't join
+        friends = list(filter(lambda x: x["joinsParty"], friends))
 
-            friends.append(
-                {
-                    "name": row[nameCol],
-                    "address": row[addressCol],
-                    "preferredTransitMode": row[transitCol],
-                }
-            )
-    elif (path.suffix == ".yaml") or (path.suffix == ".yml"):
-        import yaml
+        # sort by name
+        friends = sorted(friends, key=lambda elem: elem["name"])
 
-        with open("config/friends.yaml") as f:
-            ff = yaml.load(f, Loader=yaml.FullLoader)
+    def _establishConnection(
+        self,
+        configPath: str = None,
+    ) -> googlemaps.client.Client:
+        if (configPath is None) and (os.environ.get("APITOKEN") is None):
+            raise ValueError("configPath or APITOKEN env variable must exist.")
+        elif configPath is not None:
+            # user passes apitoken via config file
 
-        friends = ff["friends"]
+            # add parent path to path
+            path = pathlib.Path(configPath)
+            sys.path.append(str(path.parent.absolute()))
 
-    friends = sorted(friends, key=lambda elem: elem["name"])
+            # import module (infer module name from filename)
+            cfg = __import__(path.stem)
 
-    return friends
+            apitoken = cfg.apitoken
+        elif os.environ.get("APITOKEN") is not None:
+            # user passes apitoken via env variable
+            apitoken = os.environ.get("APITOKEN")
 
+        # establish connection
+        gmaps = googlemaps.Client(key=apitoken)
 
-def _establishConnection(configPath: str = None) -> googlemaps.client.Client:
-    if (configPath is None) and (os.environ.get("APITOKEN") is None):
-        raise ValueError("configPath or APITOKEN env variable must exist.")
-    elif configPath is not None:
-        # user passes apitoken via config file
+        return gmaps
 
-        # add parent path to path
-        path = pathlib.Path(configPath)
-        sys.path.append(str(path.parent.absolute()))
+    def getDirections(
+        self,
+        startAddress: str,
+        destinationAddress: str,
+        transitMode: str = "transit",
+        departureTime: Union[str, dt] = dt.now(),
+        configPath: str = None,
+    ) -> Tuple[dict, int]:
 
-        # import module (infer module name from filename)
-        cfg = __import__(path.stem)
+        # Geocoding an address
+        # geocode_start = gmaps.geocode(startAddress)
 
-        apitoken = cfg.apitoken
-    elif os.environ.get("APITOKEN") is not None:
-        # user passes apitoken via env variable
-        apitoken = os.environ.get("APITOKEN")
+        # use directions api
+        dir_results = self.gmaps.directions(
+            startAddress,
+            destinationAddress,
+            mode=transitMode,
+            departure_time=departureTime,
+        )[0]
 
-    # establish connection
-    gmaps = googlemaps.Client(key=apitoken)
+        # travel duration in seconds
+        duration = dir_results["legs"][0]["duration"]["value"]
 
-    return gmaps
+        # this is a json-like dict containing the
+        # output from the directions api
+        return dir_results, duration
 
+    def getDistMatrix(
+        self,
+        startAddresses: Union[str, List[str]],
+        destinationAddresses: Union[str, List[str]],
+        transitMode: str = "transit",
+        departureTime: Union[str, dt] = dt.now(),
+        configPath: str = None,
+    ) -> Tuple[dict, int]:
 
-def getDirections(
-    startAddress: str,
-    destinationAddress: str,
-    transitMode: str = "transit",
-    departureTime: Union[str, dt] = dt.now(),
-    configPath: str = None,
-) -> Tuple[dict, int]:
-    gmaps = _establishConnection(configPath=configPath)
+        dist_results = self.gmaps.distance_matrix(
+            startAddresses,
+            destinationAddresses,
+            mode=transitMode,
+            departure_time=departureTime,
+        )
 
-    # Geocoding an address
-    # geocode_start = gmaps.geocode(startAddress)
+        duration = dist_results["rows"][0]["elements"][0]["duration"]["value"]
 
-    # use directions api
-    dir_results = gmaps.directions(
-        startAddress,
-        destinationAddress,
-        mode=transitMode,
-        departure_time=departureTime,
-    )[0]
-
-    # travel duration in seconds
-    duration = dir_results["legs"][0]["duration"]["value"]
-
-    # this is a json-like dict containing the
-    # output from the directions api
-    return dir_results, duration
-
-
-def getDistMatrix(
-    startAddresses: Union[str, List[str]],
-    destinationAddresses: Union[str, List[str]],
-    transitMode: str = "transit",
-    departureTime: Union[str, dt] = dt.now(),
-    configPath: str = None,
-) -> Tuple[dict, int]:
-
-    gmaps = _establishConnection(configPath=configPath)
-
-    dist_results = gmaps.distance_matrix(
-        startAddresses,
-        destinationAddresses,
-        mode=transitMode,
-        departure_time=departureTime,
-    )
-
-    duration = dist_results["rows"][0]["elements"][0]["duration"]["value"]
-
-    return dist_results, duration
+        return dist_results, duration
