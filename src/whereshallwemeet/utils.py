7e5ad447deab3946072e0ba165fdcc5513e6861c
Dave Ochsenbein <dave.ochsenbein@gmail.com>
2024-05-20 10:45:15 +0200

    fixes #4

--- src/whereshallwemeet/utils.py
+++ src/whereshallwemeet/utils.py
@@ -3,6 +3,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from scipy.spatial import ConvexHull
 import numpy as np
+from shapely.geometry import Polygon
+import pointpats 
 
 def onDay(date, day=2, hour=18):
     """
@@ -22,12 +24,15 @@ def onDay(date, day=2, hour=18):
 def argmin(a):
     return min(range(len(a)), key=lambda x: a[x])
 
-def plotAddresses(lon, lat):
-    fig = px.scatter_mapbox(lat, lon,
+def plotAddresses(lon, lat, show=True):
+    fig = px.scatter_mapbox(lat=lat, lon=lon,
                         mapbox_style="carto-positron")
-    fig.show()
+    if show:
+        fig.show()
+    else:
+        return fig
 
-def plotConvexHull(polygon, bbox=False):
+def plotConvexHull(polygon, bbox=False, samples = None):
     fig = go.Figure(go.Scattermapbox(
         fill = "toself",
         lon = [p[0] for p in polygon], lat = [p[1] for p in polygon],
@@ -37,12 +42,19 @@ def plotConvexHull(polygon, bbox=False):
         fig.add_trace(px.line_mapbox(
             lon = [b[0] for b in box],
             lat = [b[1] for b in box]
-        ))
+        ).data[0])
+    if samples is not None:
+        f2=plotAddresses([s[0] for s in samples], [s[1] for s in samples], show=False)
+        fig.add_trace(f2.data[0])
     fig.update_layout(
         mapbox = {
             'style': "carto-positron"},
         showlegend = False)
     fig.show()
+
+def sampleWithinPoly(polygon, n=50):
+    pgon = Polygon(polygon)
+    return pointpats.random.poisson(pgon, size=n)
 
 def convexArea(lon, lat):
     h = ConvexHull(list(zip(lon, lat)))
