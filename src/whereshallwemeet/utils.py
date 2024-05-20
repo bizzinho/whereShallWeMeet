464dc77c3be158e8c3ee647cb254f5fe0891204c
Dave Ochsenbein <dave.ochsenbein@gmail.com>
2024-05-20 07:48:38 +0200

    utils fun

---
+++ src/whereshallwemeet/utils.py
@@ -0,0 +1,20 @@
+import datetime
+
+
+def onDay(date, day=2, hour=18):
+    """
+    Returns the date of the next given weekday after
+    the given date. For example, the date of next Monday.
+
+    NB: if it IS the day we're looking for, this returns 0.
+    consider then doing onDay(foo, day + 1).
+    """
+    days = (day - date.weekday() + 7) % 7
+
+    newdate = date + datetime.timedelta(days=days)
+
+    return newdate.replace(hour=hour, minute=0, second=0, microsecond=0)
+
+
+def argmin(a):
+    return min(range(len(a)), key=lambda x: a[x])
